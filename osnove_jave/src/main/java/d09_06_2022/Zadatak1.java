259adc03c2ee5b384fb994e2ac927012b9cf25ca
Dimitrije Petronijevic <dimitrijep94@gmail.com>
2022-06-13 18:01:55 +0200

    Upload Zadatak1 09.06.2022

--- osnove_jave/src/main/java/d09_06_2022/Zadatak1.java
+++ osnove_jave/src/main/java/d09_06_2022/Zadatak1.java
@@ -19,7 +19,7 @@ public class Zadatak1 {
 //    I izvrsite akciju klik na odgovarajuci element preko indeksa
 //    Na kraju programa ugasite pretrazivac.
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.navigate().to("https://s.bootsnipp.com/iframe/WaXlr");
@@ -31,8 +31,8 @@ public class Zadatak1 {
 
         System.out.print("Gde zelite kliknuti? (1-5): ");
         int ocena = s.nextInt();
-
         ocenjivanje.get(ocena - 1).click();
+        Thread.sleep(2000);
 
         driver.quit();
     }
